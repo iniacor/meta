51a626ce719c87fee699177a24c7c72d590c1468
ihor <evroigor86@gmail.com>
2023-03-06 14:35:34 +0200

    add download news onClick function

--- src/index.js
+++ src/index.js
@@ -3,100 +3,13 @@ import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
 import '../src/styles/index.scss';
 
-// const baseUrl = 'https://63ee0ec0388920150dd83e3c.mockapi.io/news?limit=5&page=1';
-
-// const newsList = document.querySelector('.news__list');
-// const loadNewsBtn = document.querySelector('.news-load-btn');
-
-// const state = {
-//   news: [],
-// };
-
-// const formatDate = dateStr => {
-//   const date = new Date(dateStr);
-//   return format(date, 'HH:mm - dd MMMM yyyy', { locale: ru });
-// };
-
-// const createPost = news => `
-//   <li class="news__list-item">
-//     <div class="news__item">
-//       <img
-//         class="news__item-img"
-//         loading="lazy"
-//         src=${news.imageUrl}
-//         alt="img description"
-//       />
-//       <div class="news__item-content">
-//         <div class="news__item-meta">
-//           <span class="news__item-time">${formatDate(news.createdAt)}</span>
-//           <div class="news__item-comments">
-//             <i class="icon-comments"></i>
-//             <span class="news__item-comments-quantity">${news.commentsCount}</span>
-//           </div>
-//         </div>
-//         <div class="news__item-wrapper">
-//           <a class="news__item-link" href="#">
-//             <h3 class="news__item-title">
-//               ${news.title}
-//             </h3>
-//           </a>
-//           <p class="news__item-text">
-//             ${news.content}
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   </li>
-// `;
-
-// const fillNews = news => {
-//   newsList.innerHTML = '';
-//   if (news.length) {
-//     const fragment = document.createDocumentFragment();
-//     news.map(oneNews => {
-//       const li = document.createElement('li');
-//       li.classList.add('news__list-item');
-//       li.innerHTML = createPost(oneNews);
-//       fragment.appendChild(li);
-//     });
-//     newsList.appendChild(fragment);
-//   }
-// };
-
-// const getNewsData = async () => {
-//   try {
-//     const { data } = await axios.get(baseUrl);
-//     const newsList = (state.news = state.news.concat(data));
-//     console.log(newsList);
-//     return newsList;
-//   } catch (error) {
-//     console.error(error.message);
-//     throw error;
-//   }
-// };
-
-// const handleButtonClick = async () => {
-//   try {
-//     await getNewsData();
-//     fillNews(state.news);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// loadNewsBtn.addEventListener('click', handleButtonClick);
 const baseUrl = 'https://63ee0ec0388920150dd83e3c.mockapi.io/news';
-let currentPage = 1;
-
 const newsList = document.querySelector('.news__list');
 const loadNewsBtn = document.querySelector('.news-load-btn');
-const loadMoreNewsBtn = document.createElement('button');
-loadMoreNewsBtn.classList.add('news-load-more-btn');
-loadMoreNewsBtn.innerText = 'Більше новин';
-loadMoreNewsBtn.style.display = 'none';
 
 const state = {
   news: [],
+  currentPage: 1,
 };
 
 const formatDate = dateStr => {
@@ -137,6 +50,7 @@ const createPost = news => `
 `;
 
 const fillNews = news => {
+  newsList.innerHTML = '';
   if (news.length) {
     const fragment = document.createDocumentFragment();
     news.map(oneNews => {
@@ -146,21 +60,14 @@ const fillNews = news => {
       fragment.appendChild(li);
     });
     newsList.appendChild(fragment);
-
-    if (news.length < 5) {
-      loadMoreNewsBtn.style.display = 'none';
-    } else {
-      loadMoreNewsBtn.style.display = 'block';
-      loadNewsBtn.style.display = 'none';
-    }
   } else {
-    loadMoreNewsBtn.style.display = 'none';
+    loadNewsBtn.style.display = 'none';
   }
 };
 
-const getNewsData = async (page = 1) => {
+const getNewsData = async page => {
   try {
-    const { data } = await axios.get(`${baseUrl}?limit=5&page=${page}`);
+    const { data } = await axios.get(`${baseUrl}?page=${page}&limit=5`);
     return data;
   } catch (error) {
     console.error(error.message);
@@ -168,26 +75,30 @@ const getNewsData = async (page = 1) => {
   }
 };
 
-const handleLoadMoreNewsBtnClick = async () => {
-  currentPage += 1;
-  const moreNews = await getNewsData(currentPage);
-  state.news = state.news.concat(moreNews);
-  fillNews(moreNews);
-};
-
-const handleLoadNewsBtnClick = async () => {
-  currentPage = 1;
-  state.news = await getNewsData(currentPage);
-  newsList.innerHTML = '';
-  fillNews(state.news);
-  if (state.news.length >= 5) {
-    loadMoreNewsBtn.style.display = 'block';
-  } else {
-    loadMoreNewsBtn.style.display = 'none';
+const handleButtonClick = async () => {
+  try {
+    if (state.currentPage === 0) {
+      state.news = await getNewsData(1);
+      fillNews(state.news);
+      state.currentPage = 1;
+    } else {
+      loadNewsBtn.innerText = 'Завантажую...';
+      const moreNews = await getNewsData(state.currentPage);
+      state.news = state.news.concat(moreNews);
+      fillNews(state.news);
+      state.currentPage += 1;
+      if (moreNews.length === 0) {
+        loadNewsBtn.style.display = 'none';
+      } else {
+        loadNewsBtn.style.display = 'block';
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    alert('Failed to load news. Please try again later.');
+  } finally {
+    loadNewsBtn.innerText = 'Більше новин';
   }
 };
 
-loadNewsBtn.addEventListener('click', handleLoadNewsBtnClick);
-
-loadMoreNewsBtn.addEventListener('click', handleLoadMoreNewsBtnClick);
-newsList.after(loadMoreNewsBtn);
+loadNewsBtn.addEventListener('click', handleButtonClick);
